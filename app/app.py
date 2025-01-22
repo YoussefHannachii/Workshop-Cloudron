e79893a40b5e89ce8e1beffc6248f92c6842952a
YoussefHannachii <youssef.hannachi8@gmail.com>
2025-01-22 11:58:50 +0100

    added https to the web app and adapted the dockerfiles and docker compse for that

--- app/app.py
+++ app/app.py
@@ -49,4 +49,4 @@ def delete_article(article_id):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=443, ssl_context=('cert.pem', 'key.pem'))
